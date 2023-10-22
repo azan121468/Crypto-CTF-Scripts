796cf2a3cca660ee1671309d74f797c8ee5f1e89
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2023-10-22 17:11:18 +0500

    fixed typo

--- aes.py
+++ aes.py
@@ -159,7 +159,7 @@ def decrypt(key, ciphertext):
     return plaintext
 
 key        = b'\xc3,\\\xa6\xb5\x80^\x0c\xdb\x8d\xa5z*\xb6\xfe\\'
-ciphertext = b'\xd1O\x14j\xa4+O\xb6\xa1\xc4\x08B)\x8f\x12\xdd'
+plaintext = b'\xd1O\x14j\xa4+O\xb6\xa1\xc4\x08B)\x8f\x12\xdd'
 
 aes = AES.new(key, AES.MODE_ECB)
-assert aes.encrypt(ciphertext) == encrypt(key, ciphertext)
+assert aes.encrypt(plaintext) == encrypt(key, plaintext)
