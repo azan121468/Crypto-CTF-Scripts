4c6493b76de3423b5e4a7fa1a8272a25e6c15e64
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2023-10-22 17:13:17 +0500

    Added check to show correction decryption of AES in this implementation

--- aes.py
+++ aes.py
@@ -159,7 +159,10 @@ def decrypt(key, ciphertext):
     return plaintext
 
 key        = b'\xc3,\\\xa6\xb5\x80^\x0c\xdb\x8d\xa5z*\xb6\xfe\\'
-plaintext = b'\xd1O\x14j\xa4+O\xb6\xa1\xc4\x08B)\x8f\x12\xdd'
+text = b'\xd1O\x14j\xa4+O\xb6\xa1\xc4\x08B)\x8f\x12\xdd'
 
 aes = AES.new(key, AES.MODE_ECB)
-assert aes.encrypt(plaintext) == encrypt(key, plaintext)
+assert aes.encrypt(text) == encrypt(key, text)
+
+aes = AES.new(key, AES.MODE_ECB)
+assert aes.decrypt(text) == decrypt(key, text)
